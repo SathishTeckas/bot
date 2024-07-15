d29707ef758fa8165cb2c6d7dcb706daf03c4b38
Sathish M <153342012+SathishTeckas@users.noreply.github.com>
2024-07-15 10:59:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 const TelegramBot = require('node-telegram-bot-api');
-const express = require('express');
-const http = require('http');
 
 const token = '7332835734:AAHB8ihpf2YZLru9Q4jjhEicwoFcz2w6fZE';
 const bot = new TelegramBot(token, { polling: true });
@@ -9,7 +7,9 @@ bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id;
   const name = msg.from.first_name || 'User';
 
-  bot.sendMessage(chatId, `Hello, ${name}! Welcome to GFXvs, where creators clash for daily cash prizes. Cast your vote to secure participation NFTs and a chance to win an exclusive 1:1 masterpiece. Connect your NEAR wallet to join the thrilling competition!. \nPlease click 👇 the Play button to participate in the battle.`);
+  const message = `Hello, ${name}! Welcome to GFXvs, where creators clash for daily cash prizes. Cast your vote to secure participation NFTs and a chance to win an exclusive 1:1 masterpiece. Connect your NEAR wallet to join the thrilling competition!\nPlease click 👇 the Play button to participate in the battle.`;
+
+  bot.sendMessage(chatId, message);
 });
 
 // Matches "/help"
@@ -24,22 +24,9 @@ bot.on('message', (msg) => {
   const chatId = msg.chat.id;
   const text = msg.text;
 
-  // if (text.startsWith('/')) {
-  //   bot.sendMessage(chatId, 'Unknown command. Use /help to see the list of available commands.');
-  // }
-});
-
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Telegram bot is running!');
-});
-
-const server = http.createServer(app);
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  if (text.startsWith('/')) {
+    bot.sendMessage(chatId, 'Unknown command. Use /help to see the list of available commands.');
+  }
 });
 
 bot.on('polling_error', (error) => {
